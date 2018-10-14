21d3fd1aee478654b35429f2afef413d4b2f947b
Unisov Ilya <ilya.unisov@gmail.com>
2018-10-14 23:28:42 +0300

    core/task.py changed

--- core/task.py
+++ core/task.py
@@ -11,7 +11,7 @@ import json
 
 @app.task()
 def get_md5_task(task_id):
-    Model = apps.get_model(app_label='core_old', model_name='Task')
+    Model = apps.get_model(app_label='core', model_name='Task')
     task = Model.objects.get(id=task_id)
     try:
         cxt = ssl._create_unverified_context()
@@ -28,7 +28,7 @@ def get_md5_task(task_id):
 
 @app.task()
 def send_mail_task(task_id):
-    Model = apps.get_model(app_label='core_old', model_name='Task')
+    Model = apps.get_model(app_label='core', model_name='Task')
     task = Model.objects.get(id=task_id)
     message = {
         'md5': task.md5,
